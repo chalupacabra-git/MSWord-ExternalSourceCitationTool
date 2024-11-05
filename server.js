7fe767ed424f1edd99637fcd37021bf14361c733
gcdido <61195860+gcdido@users.noreply.github.com>
2024-11-05 14:09:37 -0500

    debugging server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const path = require("path");
+const express = "express";
+const path = "path";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = PORT || 3000;
 
 // Serve static files from the 'src' directory
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join("src")));
 
 // Serve the task pane HTML file
 app.get("/taskpane", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/taskpane/taskpane.html"));
+  res.sendFile(path.join("src/taskpane/taskpane.html"));
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on porto ${PORT}`);
+  "Server is running on porto ${PORT}";
 });
