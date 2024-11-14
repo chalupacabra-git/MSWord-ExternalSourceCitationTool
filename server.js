2b92b14d7de151f711c8bd457277b6d5cf3c836a
gcdido <61195860+gcdido@users.noreply.github.com>
2024-11-14 11:39:01 -0500

    changed from server.mjs to .js and updated package URLs to point to correct locations (were previously still referencing example code). also pointed webpack to correct azure deployment (was also referencing example code).

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 import express from "express";
 import path from "path";
+import * as console from "console";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the 'src' directory
-app.use(express.static(path.join(import.meta.url, "../src")));
+app.use(express.static(path.join(__dirname, "../src")));
 
 // Serve the task pane HTML file
 app.get("/taskpane", (req, res) => {
-  res.sendFile(path.join(import.meta.url, "../src/taskpane/taskpane.html"));
+  res.sendFile(path.join(__dirname, "../src/taskpane/taskpane.html"));
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
